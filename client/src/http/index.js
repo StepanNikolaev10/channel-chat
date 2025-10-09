3b8754f3093fff9ca582862957c02640410aff85
NikolaS <nikolaevs1006@gmail.com>
2025-10-09 19:30:23 +0300

    Update backend links

--- client/src/http/index.js
+++ client/src/http/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://channelchat-production.up.railway.app/api';
 
 const api = axios.create({
     withCredentials: true,
