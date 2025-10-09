f14557eaabeb3a5f633e785d1c10d9aaf370b09d
NikolaS <nikolaevs1006@gmail.com>
2025-10-09 20:54:12 +0300

    ws back url update

--- client/src/WebSocketClient/index.js
+++ client/src/WebSocketClient/index.js
@@ -8,7 +8,7 @@ class WebSocketClient {
     connect() {
         return new Promise((resolve, reject) => {
             const token = localStorage.getItem('token');
-            this.socket = new WebSocket('ws://channelchat-production.up.railway.app');
+            this.socket = new WebSocket('wss://channelchat-production.up.railway.app');
 
             this.socket.addEventListener('open', (event) => {
                 this.socket.send(JSON.stringify({ type: 'auth', token }));
